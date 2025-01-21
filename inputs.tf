50948ed186eb887e028aa01db6d00859d7b2743a
nykzaR <nykzar@outlook.com>
2025-01-21 18:35:37 +0530

    added vars

---
+++ inputs.tf
@@ -0,0 +1,5 @@
+variable "subnets" {
+    type    = list(string)
+    default = [ "subnet1", "subnet2", "subnet3", "subnet4", "subnet5", "subnet6" ]
+}
+
--- main.tf
+++ main.tf
@@ -22,56 +22,12 @@ resource "azurerm_virtual_network" "tf_vnet" {
     depends_on          = [ azurerm_resource_group.tf_rg ]
 }
 
-resource "azurerm_subnet" "tf_subnet1" {
-    name                 = "terra-subnet1"
-    address_prefixes     = [ "10.10.0.0/24" ]
+resource "azurerm_subnet" "tf_subnets" {
+    count                = length(var.subnets)
+    name                 = var.subnets[count.index]
+    address_prefixes     = [cidrsubnet("10.10.0.0/16", 8, count.index)]
     resource_group_name  = azurerm_resource_group.tf_rg.name
     virtual_network_name = azurerm_virtual_network.tf_vnet.name
     
     depends_on           = [ azurerm_virtual_network.tf_vnet ]
-}
-
-resource "azurerm_subnet" "tf_subnet2" {
-    name                 = "terra-subnet2"
-    address_prefixes     = [ "10.10.1.0/24" ]
-    resource_group_name  = azurerm_resource_group.tf_rg.name
-    virtual_network_name = azurerm_virtual_network.tf_vnet.name
-    
-    depends_on           = [ azurerm_subnet.tf_subnet1 ]
-}
-
-resource "azurerm_subnet" "tf_subnet3" {
-    name                 = "terra-subnet3"
-    address_prefixes     = [ "10.10.2.0/24" ]
-    resource_group_name  = azurerm_resource_group.tf_rg.name
-    virtual_network_name = azurerm_virtual_network.tf_vnet.name
-    
-    depends_on           = [ azurerm_subnet.tf_subnet2]
-}
-
-resource "azurerm_subnet" "tf_subnet4" {
-    name                 = "terra-subnet4"
-    address_prefixes     = [ "10.10.3.0/24" ]
-    resource_group_name  = azurerm_resource_group.tf_rg.name
-    virtual_network_name = azurerm_virtual_network.tf_vnet.name
-    
-    depends_on           = [ azurerm_subnet.tf_subnet3 ]
-}
-
-resource "azurerm_subnet" "tf_subnet5" {
-    name                 = "terra-subnet5"
-    address_prefixes     = [ "10.10.4.0/24" ]
-    resource_group_name  = azurerm_resource_group.tf_rg.name
-    virtual_network_name = azurerm_virtual_network.tf_vnet.name
-    
-    depends_on           = [ azurerm_subnet.tf_subnet4 ]
-}
-
-resource "azurerm_subnet" "tf_subnet6" {
-    name                 = "terra-subnet6"
-    address_prefixes     = [ "10.10.5.0/24" ]
-    resource_group_name  = azurerm_resource_group.tf_rg.name
-    virtual_network_name = azurerm_virtual_network.tf_vnet.name
-    
-    depends_on           = [ azurerm_subnet.tf_subnet5 ]
 }
